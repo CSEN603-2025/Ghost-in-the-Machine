b8eee06bb6a4e60205ee9ebc775f3c1b6eac0ecd
ShahdAlaa5555 <shahdalaa003@gmail.com>
2025-05-02 02:19:36 +0400

    Student Dashboard

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import StudentDashboard from "./pages/StudentDashboard"; 
-
-
-
-
-
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterCompanyPage from './pages/RegisterCompany';
+import StudentDashboard from './pages/StudentDashboard';
+import WorkshopsPage from './pages/WorkshopsPage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register-company" element={<RegisterCompanyPage />} />
       <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/workshops" element={<WorkshopsPage />} />
     </Routes>
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import LoginPage from './pages/LoginPage';
-import WorkshopsPage from './pages/WorkshopsPage';  // <-- import your new page
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisterCompanyPage from './pages/RegisterCompany';
-import StudentDashboard from './pages/StudentDashboard';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        <Route path="/register-company" element={<RegisterCompanyPage />} />
-        <Route path="/workshops" element={<WorkshopsPage />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-      </Routes>
-    </Router>
   );
 }
-
 export default App;
