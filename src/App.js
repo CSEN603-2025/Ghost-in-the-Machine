4603fe38afe376a3acd5ef2b96392c56afc15bbb
ShahdAlaa5555 <shahdalaa003@gmail.com>
2025-05-02 16:45:35 +0400

    MyApplication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterCompanyPage from './pages/RegisterCompany';
 import StudentDashboard from './pages/StudentDashboard';
+import MyApplicationsPage from './pages/MyApplicationsPage'; // New page for My Applications
 import WorkshopsPage from './pages/WorkshopsPage';
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/register-company" element={<RegisterCompanyPage />} />
       <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/student/applications" element={<MyApplicationsPage />} /> {/* New route */}
       <Route path="/workshops" element={<WorkshopsPage />} />
     </Routes>
   );
 }
+
 export default App;
